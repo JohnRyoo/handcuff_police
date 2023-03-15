49b7c57285ca45e1e1f6f742d533269de46a33a6
JohnRyoo <hcroo@asplease.com>
2023-03-15 15:17:49 +0900

    SingleChildScrollView 내부에 stack을 사용할 수 없어 Column과 SizedBox로 대체 수정함.

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -21,6 +21,13 @@ class _LoginScreenState extends State<LoginScreen> {
   String userId = '';
   String userPassword = '';
 
+  void _validationCheck() {
+    final isValid = _formKey.currentState!.validate();
+    if (isValid) {
+      _formKey.currentState!.save();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     _phoneHeight = MediaQuery.of(context).size.height;
@@ -28,119 +35,190 @@ class _LoginScreenState extends State<LoginScreen> {
 
     return Scaffold(
       backgroundColor: Palette.backgroundColor,
-      body: GestureDetector (
+      body: GestureDetector(
         onTap: () {
           FocusScope.of(context).unfocus(); // 키보드 사라지게..
         },
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            // HANDCUFFS OPERATOR
-            Positioned(
-                top: 111,
-                child: SizedBox(
-                  width: _phoneWidth,
-                  height: 66,
-                  child: Text(
-                    'HANDCUFFS\nOPERATOR',
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.notoSans(
-                      textStyle: const TextStyle(
-                        color: Palette.headTextColor,
-                        fontSize: 24,
-                        fontWeight: FontWeight.w800,
-                        height: 1.4,
-                      ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 110,
+              ),
+              SizedBox(
+                width: _phoneWidth,
+                child: Text(
+                  'HANDCUFFS\nOPERATOR',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.notoSans(
+                    textStyle: const TextStyle(
+                      color: Palette.headTextColor,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w800,
+                      height: 1.4,
                     ),
-                  ),
-                )),
-            // 아이디 입력
-            Positioned(
-              top: 290,
-              child: SizedBox(
-                width: 320,
-                height: 60,
-                child: TextFormField(
-                  style: const TextStyle(color: Color(0xffa0a0a0)),
-                  decoration: const InputDecoration(
-                    filled: true,
-                    fillColor: Palette.darkButtonColor,
-                    prefixIcon: Icon(
-                      Icons.account_circle,
-                      color: Palette.whiteTextColor,
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      // borderSide: BorderSide(color: Color(0xffa0a0a0)),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(35.0),
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      // borderSide: BorderSide(color: Color(0xffa0a0a0)),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(35.0),
-                      ),
-                    ),
-                    hintText: '아이디',
-                    hintStyle: TextStyle(
-                      fontSize: 14,
-                      color: Palette.whiteTextColor,
-                    ),
-                    // contentPadding: EdgeInsets.all(10.0),
                   ),
                 ),
               ),
-            ),
-            // 비밀번호 입력
-            Positioned(
-              top: 360,
-              child: SizedBox(
-                width: 320,
-                height: 60,
-                child: TextFormField(
-                  style: const TextStyle(color: Color(0xffa0a0a0)),
-                  decoration: const InputDecoration(
-                    filled: true,
-                    fillColor: Palette.darkButtonColor,
-                    prefixIcon: Icon(
-                      Icons.password,
-                      color: Palette.whiteTextColor,
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      // borderSide: BorderSide(color: Color(0xffa0a0a0)),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(35.0),
+
+              const SizedBox(
+                height: 110,
+              ),
+
+              // 아이디, 비밀번호 입력
+              SizedBox(
+                // width: _phoneWidth,
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              key: const ValueKey(1),
+                              validator: (value) {
+                                if (value!.isEmpty || value.length < 4) {
+                                  return '아이디에 최소 4자 이상을 입력하세요.';
+                                }
+                                return null;
+                              },
+                              onSaved: (value) {
+                                userId = value!;
+                              },
+                              onChanged: (value) {
+                                userId = value;
+                              },
+                              style: const TextStyle(
+                                  color: Palette.darkButtonColor),
+                              decoration: const InputDecoration(
+                                filled: true,
+                                fillColor: Palette.darkButtonColor,
+                                prefixIcon: Icon(
+                                  Icons.account_circle,
+                                  color: Palette.whiteTextColor,
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(35.0),
+                                  ),
+                                ),
+                                errorBorder: OutlineInputBorder(
+                                  // borderSide: BorderSide(
+                                  //   color: Colors.red,
+                                  // ),
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(35.0),
+                                  ),
+                                ),
+                               focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(35.0),
+                                  ),
+                                ),
+                                focusedErrorBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(35.0),
+                                  ),
+                                ),
+                                hintText: '아이디',
+                                hintStyle: TextStyle(
+                                  fontSize: 14,
+                                  color: Palette.whiteTextColor,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            TextFormField(
+                              key: const ValueKey(2),
+                              obscureText: true,
+                              validator: (value) {
+                                if (value!.isEmpty || value.length < 6) {
+                                  return "비밀번호는 최소 7자리 이상입니다.";
+                                }
+                                return null;
+                              },
+                              onSaved: (value) {
+                                userPassword = value!;
+                              },
+                              onChanged: (value) {
+                                userPassword = value;
+                              },
+                              style: const TextStyle(
+                                  color: Palette.darkButtonColor),
+                              decoration: const InputDecoration(
+                                filled: true,
+                                fillColor: Palette.darkButtonColor,
+                                prefixIcon: Icon(
+                                  Icons.password,
+                                  color: Palette.whiteTextColor,
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(35.0),
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(35.0),
+                                  ),
+                                ),
+                                errorBorder: OutlineInputBorder(
+                                  // borderSide: BorderSide(
+                                  //   color: Colors.red,
+                                  // ),
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(35.0),
+                                  ),
+                                ),
+                                focusedErrorBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(35.0),
+                                  ),
+                                ),
+                                hintText: '비밀번호',
+                                hintStyle: TextStyle(
+                                  fontSize: 14,
+                                  color: Palette.whiteTextColor,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      // borderSide: BorderSide(color: Color(0xffa0a0a0)),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(35.0),
-                      ),
-                    ),
-                    hintText: '비밀번호',
-                    hintStyle: TextStyle(
-                      fontSize: 14,
-                      color: Palette.whiteTextColor,
-                    ),
-                    // contentPadding: EdgeInsets.all(10.0),
-                  ),
+                  ],
                 ),
               ),
-            ),
-            // 로그인
-            Positioned(
-              top: 500,
-              child: Center(
+
+              const SizedBox(
+                height: 90,
+              ),
+
+              // 로그인 버튼
+              Container(
+                // width: _phoneWidth,
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
                 child: GestureDetector(
                   onTap: () {
-                    Navigator.push(context,
+                    if (_formKey.currentState!.validate()) {
+                      Navigator.push(
+                        context,
                         MaterialPageRoute(builder: (context) {
-                      return MainPageScreen();
-                    }));
+                          return MainPageScreen();
+                        }),
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text('아이디를 확인해 주세요!'),
+                        backgroundColor: Colors.blue,
+                      ));
+                    }
                   },
                   child: Container(
-                    width: 320,
                     height: 60,
                     decoration: BoxDecoration(
                       color: Palette.lightButtonColor,
@@ -164,11 +242,15 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ),
               ),
-            ),
-            // 회원가입 버튼
-            Positioned(
-              top: 570,
-              child: Center(
+
+              const SizedBox(
+                height: 10,
+              ),
+
+              // 회원가입 버튼
+              Container(
+                // width: _phoneWidth,
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
                 child: GestureDetector(
                   onTap: () {
                     Navigator.push(context,
@@ -177,7 +259,6 @@ class _LoginScreenState extends State<LoginScreen> {
                     }));
                   },
                   child: Container(
-                    width: 320,
                     height: 60,
                     decoration: BoxDecoration(
                       border: Border.all(color: Palette.lightButtonColor),
@@ -201,29 +282,25 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ),
               ),
-            ),
-            // Copyright
-            Positioned(
-              top: 769,
-              child: Align(
-                child: SizedBox(
-                  width: _phoneWidth,
-                  height: 17,
-                  child: Text(
-                    'Copyright © TERRAONE Corp. All Rights Reserved.',
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.notoSans(
-                        textStyle: const TextStyle(
-                      color: Palette.whiteTextColor,
-                      fontSize: 12,
-                      fontWeight: FontWeight.w400,
-                      height: 1.4,
-                    )),
-                  ),
-                ),
+
+              const SizedBox(
+                height: 150,
               ),
-            ),
-          ],
+
+              // Copyright
+              Text(
+                'Copyright © TERRAONE Corp. All Rights Reserved.',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.notoSans(
+                    textStyle: const TextStyle(
+                  color: Palette.whiteTextColor,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  height: 1.4,
+                )),
+              ),
+            ],
+          ),
         ),
       ),
     );
